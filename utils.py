57ce45341261c65130b1b0e92beeb47462cb419e
Mati-Muli <m.mulewicz89@gmail.com>
2026-05-28 17:30:36 +0200

    Zad 1

--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
+"""Simple Calculator"""
 def add(a: int, b: int) -> int:
+    """Adding two intigers"""
     return a+b
 
 def subtract(a: int, b: int) -> int:
+    """Subtracting two intigers"""
     return a-b
 
 def multiply(a: int, b: int) -> int:
+    """Multiplying two intigers"""
     return a*b
 
 def divide(a: int, b: int) -> int:
+    """Dividing two intigers"""
     return a/b
