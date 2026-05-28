61a999447df5483ba1814dda665c7ebd19e83acb
Mati-Muli <m.mulewicz89@gmail.com>
2026-05-28 17:32:56 +0200

    Reformat

--- utils.py
+++ utils.py
@@ -1,16 +1,21 @@
 """Simple Calculator"""
+
+
 def add(a: int, b: int) -> int:
     """Adding two intigers"""
-    return a+b
+    return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Subtracting two intigers"""
-    return a-b
+    return a - b
+
 
 def multiply(a: int, b: int) -> int:
     """Multiplying two intigers"""
-    return a*b
+    return a * b
+
 
 def divide(a: int, b: int) -> int:
     """Dividing two intigers"""
-    return a/b
+    return a / b
