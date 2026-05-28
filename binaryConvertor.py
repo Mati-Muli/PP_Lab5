5b14e22d51c7315e1ab48f6945f863e2fe7e1396
Mati-Muli <m.mulewicz89@gmail.com>
2026-05-28 17:55:07 +0200

    Zad 3

--- binaryConvertor.py
+++ binaryConvertor.py
@@ -4,6 +4,6 @@
 def convert(n: int) -> str:
     if not isinstance(n, int):
         raise TypeError("n must be an integer")
-    if 0 < n < 100:
+    if (0 < n < 100):
         raise ValueError("n must be between 0 and 100")
     return bin(n)[2:]
