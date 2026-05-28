4a010cd750fd11e1e9a4fac36d4209a8d5a87fa3
Mati-Muli <m.mulewicz89@gmail.com>
2026-05-28 18:08:16 +0200

    Poprawka

--- binaryConvertor.py
+++ binaryConvertor.py
@@ -3,7 +3,7 @@
 
 def convert(n: int) -> str:
     if not isinstance(n, int):
-        raise TypeError("n must be an integer")
+        raise ValueError("n must be an integer")
     if not (0 <= n <= 100):
         raise ValueError("n must be between 0 and 100")
 
